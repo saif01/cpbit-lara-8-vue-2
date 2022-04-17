07ab8dbc4914243c954deeffa1f7e418cd2fb25e
unknown <syful.cse.bd@gmail.com>
2022-04-17 22:16:34 +0600

    up over all

--- resources/js/cms/admin_hardware/pages/dashboard.vue
+++ resources/js/cms/admin_hardware/pages/dashboard.vue
@@ -77,128 +77,135 @@
                 </v-card>
             </v-col>
         </v-row>
-        
+
         <h3 class="my-5">Category Wise Complain</h3>
-        
+
         <v-row>
-            <v-col cols="12" lg="3"  v-for="(item, index) in productWiseComplain" :key="index">
-                <v-card>
-                    <v-img
-                    class="white--text align-end indigo"
-                    height="50px"
-                    >
-                    <div class="d-flex justify-content-between">
-                        <v-card-title v-text="item.category.name"></v-card-title>
-                        
-                        <v-card-title v-text="item.total" class="align-end"></v-card-title>
-                    </div>
-                    </v-img>
-                </v-card>
-            </v-col>
+            <div v-if="productWiseComplain.length > 0">
+                <v-col cols="12" lg="3" v-for="(item, index) in productWiseComplain" :key="index">
+                    <v-card>
+                        <v-img class="white--text align-end indigo" height="50px">
+                            <div class="d-flex justify-content-between">
+                                <v-card-title v-text="item.category.name"></v-card-title>
+
+                                <v-card-title v-text="item.total" class="align-end"></v-card-title>
+                            </div>
+                        </v-img>
+                    </v-card>
+                </v-col>
+            </div>
+            <div v-if="!overlay && productWiseComplain.length <= 0" class="m-auto">
+                <p class="text-center h3 text-danger"> Complain Data Not Available </p>
+            </div>
         </v-row>
 
 
         <h3 class="my-5">Damage Wise Complain</h3>
+        <div v-if="damageWiseComplain.length > 0">
+            <v-row>
+                <v-col cols="12" lg="3" v-for="(item, index) in damageWiseComplain" :key="index">
+                    <v-card>
+                        <v-img class="white--text align-end indigo" height="50px">
+                            <div class="d-flex justify-content-between">
+                                <v-card-title v-text="item.damaged_type"></v-card-title>
 
-        <v-row>
-            <v-col cols="12" lg="3"  v-for="(item, index) in damageWiseComplain" :key="index">
-                <v-card>
-                    <v-img
-                    class="white--text align-end indigo"
-                    height="50px"
-                    >
-                    <div class="d-flex justify-content-between">
-                        <v-card-title v-text="item.damaged_type"></v-card-title>
-                        
-                        <v-card-title v-text="item.total" class="align-end"></v-card-title>
-                    </div>
-                    </v-img>
-                </v-card>
-            </v-col>
-        </v-row>
+                                <v-card-title v-text="item.total" class="align-end"></v-card-title>
+                            </div>
+                        </v-img>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </div>
+        <div v-if="!overlay && productWiseComplain.length <= 0" class="m-auto">
+            <p class="text-center h3 text-danger"> Complain Data Not Available </p>
+        </div>
+
+        <v-overlay :value="overlay">
+            <v-progress-circular indeterminate size="64"></v-progress-circular>
+        </v-overlay>
 
     </div>
 </template>
 
 <script>
+    export default {
+
+        data() {
+            return {
+                // current Url
+                currentUrl: '/cms/h_admin',
+                productWiseComplain: '',
+                allComplain: '',
+                damageWiseComplain: '',
+                value: '100',
+                dashboardData: '',
 
 
 
-export default {
-    
-    data(){
-        return{
-            // current Url
-            currentUrl: '/cms/h_admin',
-            productWiseComplain: '',
-            allComplain: '',
-            damageWiseComplain: '',
-            value: '100',
-            dashboardData: '',
-           
+            }
+        },
 
-         
+        methods: {
+            getDashboardData() {
+                this.overlay = true
+                axios.get(this.currentUrl + '/dashboard_data').then(response => {
+
+                    this.overlay = false
+                    this.productWiseComplain = response.data.productWiseComplain
+                    this.dashboardData = response.data
+
+                    this.damageWiseComplain = response.data.damageWiseComplain
+
+                    console.log(damageWiseComplain);
+
+
+                    // let chData = response.data.chartData
+                    // // For chart
+                    // if(chData){
+
+                    //     let chlabels = [];
+                    //     let chdata = [];
+                    //     chData.forEach(item=>{
+                    //         if(item.category){
+                    //             chlabels.push(item.category.name)
+                    //         }
+                    //         chdata.push(item.total)
+                    //         // console.log('item', item.total, item.category.name)
+                    //     })
+
+                    //     let dataArry = { labels: chlabels, data: chdata }
+                    //     // assign dara 
+                    //     this.allChartData = dataArry;
+                    //     this.chartShow = true;
+                    //     // console.log(this.allChartData)
+                    // }
+                }).catch(error => {
+                    this.overlay = false
+                    console.log(error)
+                })
+            }
+
+
+        },
+
+        created() {
+
+            this.$Progress.start();
+            this.getDashboardData()
+            this.$Progress.finish();
         }
-    },
-
-    methods:{
-        getDashboardData(){
-            axios.get( this.currentUrl + '/dashboard_data').then(response=>{
-                this.productWiseComplain = response.data.productWiseComplain
-                this.dashboardData = response.data
-
-                this.damageWiseComplain = response.data.damageWiseComplain
-
-                console.log(damageWiseComplain);
-
-
-                // let chData = response.data.chartData
-                // // For chart
-                // if(chData){
-
-                //     let chlabels = [];
-                //     let chdata = [];
-                //     chData.forEach(item=>{
-                //         if(item.category){
-                //             chlabels.push(item.category.name)
-                //         }
-                //         chdata.push(item.total)
-                //         // console.log('item', item.total, item.category.name)
-                //     })
-
-                //     let dataArry = { labels: chlabels, data: chdata }
-                //     // assign dara 
-                //     this.allChartData = dataArry;
-                //     this.chartShow = true;
-                //     // console.log(this.allChartData)
-                // }
-            }).catch(error=>{
-                console.log(error)
-            })
-        }
-
-      
-    },
-
-    created(){
-        
-        this.$Progress.start();
-        this.getDashboardData()
-        this.$Progress.finish();
     }
-}
+
 </script>
 
 
 
 <style scoped>
+    .bg_card {
+        background: linear-gradient(120deg, rgb(155, 198, 159) 60%, rgb(137, 168, 227) 40%);
+        border: 5px solid transparent;
+        border-image: url("data:image/svg+xml;charset=utf-8,%3Csvg width='100' height='100' viewBox='0 0 100 100' fill='none' xmlns='http://www.w3.org/2000/svg'%3E %3Cstyle%3Epath%7Banimation:stroke 10s infinite linear%3B%7D%40keyframes stroke%7Bto%7Bstroke-dashoffset:776%3B%7D%7D%3C/style%3E%3ClinearGradient id='g' x1='0%25' y1='0%25' x2='0%25' y2='100%25'%3E%3Cstop offset='0%25' stop-color='%232d3561' /%3E%3Cstop offset='25%25' stop-color='%23c05c7e' /%3E%3Cstop offset='50%25' stop-color='%23f3826f' /%3E%3Cstop offset='100%25' stop-color='%23ffb961' /%3E%3C/linearGradient%3E %3Cpath d='M1.5 1.5 l97 0l0 97l-97 0 l0 -97' stroke-linecap='square' stroke='url(%23g)' stroke-width='3' stroke-dasharray='388'/%3E %3C/svg%3E") 1;
 
-
-.bg_card{
-    background: linear-gradient(120deg, rgb(155, 198, 159) 60%, rgb(137, 168, 227) 40%);
-    border:5px solid transparent;
-    border-image: url("data:image/svg+xml;charset=utf-8,%3Csvg width='100' height='100' viewBox='0 0 100 100' fill='none' xmlns='http://www.w3.org/2000/svg'%3E %3Cstyle%3Epath%7Banimation:stroke 10s infinite linear%3B%7D%40keyframes stroke%7Bto%7Bstroke-dashoffset:776%3B%7D%7D%3C/style%3E%3ClinearGradient id='g' x1='0%25' y1='0%25' x2='0%25' y2='100%25'%3E%3Cstop offset='0%25' stop-color='%232d3561' /%3E%3Cstop offset='25%25' stop-color='%23c05c7e' /%3E%3Cstop offset='50%25' stop-color='%23f3826f' /%3E%3Cstop offset='100%25' stop-color='%23ffb961' /%3E%3C/linearGradient%3E %3Cpath d='M1.5 1.5 l97 0l0 97l-97 0 l0 -97' stroke-linecap='square' stroke='url(%23g)' stroke-width='3' stroke-dasharray='388'/%3E %3C/svg%3E") 1;
-    
-}
+    }
 
 </style>
--- resources/js/inventory/admin/pages/dashboard.vue
+++ resources/js/inventory/admin/pages/dashboard.vue
@@ -2,44 +2,54 @@
     <div>
         <div class="pb-6 rounded success pr-1 pl-1 pt-2">
             <h3 class="text-center pb-2 text-white">Remaining Stock</h3>
-            <v-row>
-                <v-col cols="12" lg="3" v-for="(item, index) in allDashboardData" :key="index">
-                    <v-card>
-                        <v-card-text>
-                            <p v-if="item.category">{{ item.category.name }}:
-                                <b class="float-right">{{ item.total }}</b></p>
-                        </v-card-text>
-                    </v-card>
-                </v-col>
-            </v-row>
+            <div v-if="allDashboardData.length > 0">
+                <v-row>
+                    <v-col cols="12" lg="3" v-for="(item, index) in allDashboardData" :key="index">
+                        <v-card>
+                            <v-card-text>
+                                <p v-if="item.category">{{ item.category.name }}:
+                                    <b class="float-right">{{ item.total }}</b></p>
+                            </v-card-text>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </div>
+            <div v-if="!overlay && allDashboardData.length <= 0" class="m-auto">
+                <p class="text-center h3 text-danger"> Data Not Available </p>
+            </div>
         </div>
 
 
         <!-- Operation Wise Product -->
         <div class="pb-6 rounded indigo pr-1 pl-1 mt-2 pt-2">
             <h3 class="text-center pb-2 text-white">Operation Wise Product</h3>
-            <v-row>
-                <v-col cols="12" lg="4" v-for="(item, index) in operationWise" :key="index">
-                    <v-card>
-                        <v-card-title> {{ item.lavel }} </v-card-title>
-                        <v-card height="150" class="scroll">
-                            <v-card-text v-if="item.val.length">
-                                <div v-for="(item2, index) in item.val" :key="index">
-                                    <p v-if="item2.category">{{ item2.category.name }}:
-                                        <b class="float-right">{{ item2.total }}</b></p>
-                                    <hr>
-                                </div>
-                            </v-card-text>
-                            <v-card-text v-else class="error--text text-center">
-                                <p class="h3">No Data Available</p>
-                                <v-icon x-large color="error">mdi-alert-octagon-outline</v-icon>
-                            </v-card-text>
+            <div v-if="operationWise.length > 0">
+                <v-row>
+                    <v-col cols="12" lg="4" v-for="(item, index) in operationWise" :key="index">
+                        <v-card>
+                            <v-card-title> {{ item.lavel }} </v-card-title>
+                            <v-card height="150" class="scroll">
+                                <v-card-text v-if="item.val.length">
+                                    <div v-for="(item2, index) in item.val" :key="index">
+                                        <p v-if="item2.category">{{ item2.category.name }}:
+                                            <b class="float-right">{{ item2.total }}</b></p>
+                                        <hr>
+                                    </div>
+                                </v-card-text>
+                                <v-card-text v-else class="error--text text-center">
+                                    <p class="h3">No Data Available</p>
+                                    <v-icon x-large color="error">mdi-alert-octagon-outline</v-icon>
+                                </v-card-text>
+                            </v-card>
+
+
                         </v-card>
-
-
-                    </v-card>
-                </v-col>
-            </v-row>
+                    </v-col>
+                </v-row>
+            </div>
+            <div v-if="!overlay && operationWise.length <= 0" class="m-auto">
+                <p class="text-center h3 text-danger"> Data Not Available </p>
+            </div>
         </div>
 
         <v-overlay :value="overlay">
@@ -49,10 +59,9 @@
 </template>
 
 <script>
-   
     export default {
 
-      
+
         data() {
             return {
                 // current Url
